1179f86ac5d1ddeb9b3539ed11b1560456207a0d
He Ma <hema2022@student.ubc.ca>
2024-01-12 14:05:27 -0800

    Add docstrings for select rgb channel

--- src/image_modifier/image_modifier.py
+++ src/image_modifier/image_modifier.py
@@ -23,3 +23,29 @@ def rotate_90(image):
     ]
 
     """
+
+def select_channel(path, channel, without=False):
+    """
+    Displays an image with/without a specified RGB channel modification and saves the processed image.
+
+    Parameters:
+    - path (str): Path to the image file.
+    - channel (str): The color channel to interact with. Valid options are 'r' for red, 'g' for green, or 'b' for blue.
+    - without (bool, optional): If True, removes the specified channel from the image before saving.
+      Defaults to False.
+
+    Returns:
+    - None: The function displays the modified image using matplotlib and saves the modified image to a new file.
+
+    The function reads an image from the given path, processes it based on the specified channel, 
+    and displays the modified image. It also saves this modified image in the same directory as 
+    the original, with the suffix '_modified'.
+
+    Example:
+    To display and save an image with green channel selected:
+    >>> select_channel('path_to_image.jpg', 'g', without=False)
+    
+    To display and save an image with the red channel removed:
+    >>> select_channel('path_to_image.jpg', 'r', without=True)
+
+    """
