67ffc3ee00918ef9372574c8d447d89bba2775a7
He Ma <hema2022@student.ubc.ca>
2024-01-12 21:39:04 -0800

    Modify the select_channel function

--- src/image_modifier/select_channel.py
+++ src/image_modifier/select_channel.py
@@ -1,25 +1,23 @@
-def select_channel(path, channel, without=False):
+def select_channel(image, channel, without=False):
     """
-    Displays an image with/without a specified RGB channel modification and saves the processed image.
+    Modify an image with/without a specified RGB channel.
 
     Parameters:
-    - path (str): Path to the image file.
+    - image (list of lists): A 2D list representing the original image.
     - channel (str): The color channel to interact with. Valid options are 'r' for red, 'g' for green, or 'b' for blue.
-    - without (bool, optional): If True, removes the specified channel from the image before saving.
-      Defaults to False.
+    - without (bool, optional): If True, removes the specified channel from the image. Defaults to False.
 
     Returns:
-    - None: The function displays the modified image using matplotlib and saves the modified image to a new file.
+    - list of lists: A new 2D list representing the image with the modified channel.
 
-    The function reads an image from the given path, processes it based on the specified channel, 
-    and displays the modified image. It also saves this modified image in the same directory as 
-    the original, with the suffix '_modified'.
+    The function processes an image based on the specified channel, 
+    and displays the modified image. 
 
     Example:
-    To display and save an image with green channel selected:
+    To modify an image with green channel selected:
     >>> select_channel('path_to_image.jpg', 'g', without=False)
     
-    To display and save an image with the red channel removed:
-    >>> select_channel('path_to_image.jpg', 'r', without=True)
+    To modify an image with the red channel removed:
+    >>> select_channel(image, 'r', without=True)
 
     """
